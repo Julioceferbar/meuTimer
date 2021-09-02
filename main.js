e2391b7fb9deeb6440a68e1c6a9fe28dcba1606f
Julio César <julioceferbar@gmail.com>
2021-09-02 11:15:22 -0300

    atalizando

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-alert('Olá!....')
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
