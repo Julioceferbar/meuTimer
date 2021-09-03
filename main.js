346fc27ba763bdb3236c15aedc2198be4795b1f7
Julio César <julioceferbar@gmail.com>
2021-09-03 09:33:31 -0300

    atualizando2

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const relogio = document.querySelector('.relogio');
+const relogio = document.querySelector('.relogio'); //seleciona o botao
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+
+iniciar.addEventListener('click', function(event) {
+    alert('cliquei no iniciar');
+});
+pausar.addEventListener('click', function(event) {
+    alert('cliquei no pausar');
+});
+zerar.addEventListener('click', function(event) {
+    alert('cliquei no zerar');
+});
